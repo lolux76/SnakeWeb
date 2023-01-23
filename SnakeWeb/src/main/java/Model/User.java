627f75a42f84d2801bce90f49a2e3c4d4db1030a
Anthony-Guillot <etud@port-dell-precision-7530-20>
2023-01-23 11:25:45 +0100

    Adding user model

---
+++ SnakeWeb/src/main/java/Model/User.java
@@ -0,0 +1,25 @@
+package Model;
+public class User {
+	String uuid;
+	String Nom;
+	String password;
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
